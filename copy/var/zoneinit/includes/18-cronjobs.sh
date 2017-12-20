8423121831effdaa669d56bab9a5811cd2cf4dd1
jfqd <jfqd@blun.org>
2017-12-20 12:23:26 +0100

    add syslog.conf and ipf.conf for base images

--- copy/var/zoneinit/includes/18-cronjobs.sh
+++ copy/var/zoneinit/includes/18-cronjobs.sh
@@ -1,5 +1,6 @@
 # create cronjob for ssl-expire script
 CRON='0 9 * * 1 /opt/qutic/bin/ssl-expire.sh
+15 3 * * 6 /opt/local/sbin/pkg_admin audit
 15 * * * * /opt/qutic/bin/check-log /var/adm/messages "(znapzend.*ERROR)"
 '
 (crontab -l 2>/dev/null || true; echo "$CRON" ) | sort | uniq | crontab
