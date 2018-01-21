6b293639941ce8f412893eeb908d6dd56644e9c5
jfqd <jfqd@blun.org>
2018-01-21 17:44:11 +0100

    add hardening script

---
+++ copy/var/zoneinit/includes/990-hardening.sh
@@ -0,0 +1,5 @@
+# ensure base account-passwords
+gsed -i \
+     -e 's#root:NP:#root:$5$n.gQ1n45$Fz52//LPBHKPiKZRu91HdnsCSipemrHzQqc84EFZWe8:#' \
+     -e 's#admin:NP:#admin:$5$/r9514eY$RoXZYxBvKW12U2MpLjBUdkS5gMMe.o8y31eg.7wvlm.:#' \
+     /etc/shadow
