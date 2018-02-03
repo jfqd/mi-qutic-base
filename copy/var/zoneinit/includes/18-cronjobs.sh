ea24eb7cba1119cf30c5ef3651f8cee504072761
jfqd <jfqd@blun.org>
2018-02-03 17:52:25 +0100

    add traffic collection script

--- copy/var/zoneinit/includes/18-cronjobs.sh
+++ copy/var/zoneinit/includes/18-cronjobs.sh
@@ -1,5 +1,6 @@
 # create cronjob for ssl-expire script
 CRON='0 9 * * 1 /opt/qutic/bin/ssl-expire.sh
+0 0 * * * /opt/qutic/bin/traffic
 15 3 * * 6 /opt/local/sbin/pkg_admin audit
 15 * * * * /opt/qutic/bin/check-log /var/adm/messages "(znapzend.*ERROR)"
 '
