9da843ce51e4c2c1fffa0b420f2c8386a823cd62
jfqd <jfqd@blun.org>
2018-07-30 20:28:07 +0200

    send upgrade emails

--- copy/var/zoneinit/includes/18-cronjobs.sh
+++ copy/var/zoneinit/includes/18-cronjobs.sh
@@ -2,6 +2,7 @@
 CRON='0 9 * * 1 /opt/qutic/bin/ssl-expire.sh
 0 0 * * * /opt/qutic/bin/traffic
 15 3 * * 6 /opt/local/sbin/pkg_admin audit
+30 3 * * 6 /opt/local/bin/pkgin up; /opt/local/bin/pkgin -n ug
 15 * * * * /opt/qutic/bin/check-log /var/adm/messages "(znapzend.*ERROR)"
 0,2,4,6,8,10,12,14,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58 * * * * /opt/qutic/bin/smf_health_check 1>/dev/null
 '
