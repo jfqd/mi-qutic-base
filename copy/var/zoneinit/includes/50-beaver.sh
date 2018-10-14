d090ee5417cf1f2ff6e85568ffdde4547049be08
jfqd <jfqd@blun.org>
2018-10-14 19:05:11 +0200

    ensure beaver service is running

--- copy/var/zoneinit/includes/50-beaver.sh
+++ copy/var/zoneinit/includes/50-beaver.sh
@@ -1,2 +1,4 @@
 # Enable beaver service
 /usr/sbin/svcadm enable svc:/application/management/beaver:default
+# ensure beaver service is running
+/usr/sbin/svcadm clear svc:/application/management/beaver:default
