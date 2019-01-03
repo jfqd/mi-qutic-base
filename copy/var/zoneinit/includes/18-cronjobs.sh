3acaa03065c4c94a6851939831e3f3fcfe4cb29f
jfqd <jfqd@blun.org>
2019-01-03 15:46:28 +0100

    add cronjob for log cleanup

--- copy/var/zoneinit/includes/18-cronjobs.sh
+++ copy/var/zoneinit/includes/18-cronjobs.sh
@@ -8,3 +8,4 @@ CRON='0 9 * * 1 /opt/qutic/bin/ssl-expire.sh
 (crontab -l 2>/dev/null || true; echo "$CRON" ) | sort | uniq | crontab
 
 echo '0,2,4,6,8,10,12,14,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58 * * * * /opt/qutic/bin/smf_health_check 1>/dev/null' >> /var/spool/cron/crontabs/root
+echo '0 0 * * * if [[ -d /var/log/httpd ]]; then /usr/bin/ls -1dt /var/log/httpd/access* | /usr/bin/tail -n +8 | /usr/bin/xargs /usr/bin/rm -f; fi' >> /var/spool/cron/crontabs/root
