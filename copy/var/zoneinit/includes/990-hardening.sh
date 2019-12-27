f2ff1654fe593a91e16bd4ae50280f9cda532040
jfqd <jfqd@blun.org>
2019-12-27 18:56:06 +0100

    cleanup mdata-store after zoneinit

--- copy/var/zoneinit/includes/990-hardening.sh
+++ copy/var/zoneinit/includes/990-hardening.sh
@@ -5,3 +5,11 @@ gsed -i \
      -e 's#root:NP:#root:$5$n.gQ1n45$Fz52//LPBHKPiKZRu91HdnsCSipemrHzQqc84EFZWe8:#' \
      -e 's#admin:NP:#admin:$5$/r9514eY$RoXZYxBvKW12U2MpLjBUdkS5gMMe.o8y31eg.7wvlm.:#' \
      /etc/shadow
+
+# cleanup mdata-store
+mdata-delete mail_smarthost || true
+mdata-delete mail_auth_user || true
+mdata-delete mail_auth_pass || true
+mdata-delete mail_adminaddr || true
+mdata-delete admin_authorized_keys || true
+mdata-delete root_authorized_keys || true
