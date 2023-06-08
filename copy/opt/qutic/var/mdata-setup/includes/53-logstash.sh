d9ebff6ad09c6a6ec90b12a5d3abc4721985a68b
jfqd <jfqd@blun.org>
2023-06-08 22:03:42 +0200

    add flag to sed

--- copy/opt/qutic/var/mdata-setup/includes/53-logstash.sh
+++ copy/opt/qutic/var/mdata-setup/includes/53-logstash.sh
@@ -5,6 +5,6 @@ if mdata-get logstash_redis 1>/dev/null 2>&1; then
   LOGSTASH_REDIS=$(mdata-get logstash_redis | sed "s/,/\",\"/")
 
   sed -i \
-      "s#127.0.0.1#${LOGSTASH_REDIS}#" \
+      -e "s#127.0.0.1#${LOGSTASH_REDIS}#" \
       /opt/local/etc/beats/filebeat.yml
 fi
