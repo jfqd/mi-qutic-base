bdab9198d44813c4bd412f1fe4416831d76c06a2
jfqd <jfqd@blun.org>
2023-08-16 16:48:38 +0200

    restart filebeat

--- copy/opt/qutic/var/mdata-setup/includes/53-logstash.sh
+++ copy/opt/qutic/var/mdata-setup/includes/53-logstash.sh
@@ -5,6 +5,9 @@ if mdata-get logstash_redis 1>/dev/null 2>&1; then
   LOGSTASH_REDIS=$(mdata-get logstash_redis | sed "s/,/\",\"/")
 
   sed -i \
-      -e "s#127.0.0.1#${LOGSTASH_REDIS}#" \
+      -e "s#\[\"127.0.0.1\"\]#\[\"${LOGSTASH_REDIS}#\"\]" \
+      -e "s#\[\"131.0.0.1\"\]#\[\"${LOGSTASH_REDIS}#\"\]" \
       /opt/local/etc/beats/filebeat.yml
+
+  svcadm restart svc:/pkgsrc/beats:filebeat || true
 fi
